9280c5c8174d665312a29f943d58dd055fac388f
Behruz <behruzparpiyev20@gmail.com>
2022-06-29 15:00:03 +0500

    added works model

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import BlogModel, CategoryModel
 
+@admin.register(CategoryModel)
+class categoryModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+@admin.register(BlogModel)
+class BlogModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_display_links = ['title', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'body']
 
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ckeditor',
+
     'blogs',
     'works',
     'main',
@@ -136,3 +139,18 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+
+CKEDITOR_FILENAME_GENERATOR = 'utils.get_filename'
+
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        # 'height': 300,
+        # 'width': 300,
+    },
+}
+
